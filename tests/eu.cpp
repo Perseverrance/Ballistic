724dfeaf8d0a2b77dd584c4cc60823568518a865
JumpIntoSpace <etotupouspeh@mail.ru>
2023-09-28 20:40:01 +0300

    tests included

---
+++ tests/eu.cpp
@@ -0,0 +1,37 @@
+//
+// Created by perseverance on 22.09.23.
+//
+#include <iostream>
+
+enum PCState
+{
+    ON,
+    OFF,
+    SLEEP
+};
+
+int main()
+{
+    PCState s;
+    if(s == PCState::OFF);
+//    signed char num1{ -64 };
+//    unsigned char num2{ 64 };
+//    short num3{ -88 };
+//    unsigned short num4{ 88 };
+//    int num5{ -1024 };
+//    unsigned int num6{ 1024 };
+//    long num7{ -2048 };
+//    unsigned long num8{ 2048 };
+//    long long num9{ -4096 };
+//    unsigned long long num10{ 4096 };
+//    std::cout << "num1 = " << num1 << std::endl;
+//    std::cout << "num2 = " << num2 << std::endl;
+//    std::cout << "num3 = " << num3 << std::endl;
+//    std::cout << "num4 = " << num4 << std::endl;
+//    std::cout << "num5 = " << num5 << std::endl;
+//    std::cout << "num6 = " << num6 << std::endl;
+//    std::cout << "num7 = " << num7 << std::endl;
+//    std::cout << "num8 = " << num8 << std::endl;
+//    std::cout << "num9 = " << num9 << std::endl;
+//    std::cout << "num10 = " << num10 << std::endl;
+}
---
+++ tests/test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by perseverance on 22.09.23.
+//
+#include <iostream>
+#include <gtest/gtest.h>
+#include "../src/time.h"
+
+TEST(first, first)
+{
+    //Time<Scale::UTC> time1(5, 6, Scale::UTC);
+    std::cout << "I'm alive" << std::endl;
+}
